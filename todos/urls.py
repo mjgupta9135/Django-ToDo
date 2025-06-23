df9c82d2d88c31bb0b797a9bfb11daa372be7e09
Mrityunjay <mjguptacse@gmail.com>
2025-06-23 22:34:41 +0530

    Task is Done

--- todos/urls.py
+++ todos/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     path('mark_as_done/<int:pk>/', views.mark_as_done,name='mark_as_done'),
     path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
     # # Edit Feature
-    # path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
     # Delete task
     path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 
